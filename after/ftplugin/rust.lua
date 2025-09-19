b0295d3c35f8ef153ac22cfa11e094aed7bdf033
sangook <ksangook@gmail.com>
2025-09-20 01:33:00 +0900

    ^^

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -6,13 +6,12 @@ local bufnr = vim.api.nvim_get_current_buf()
 -- end, { silent = true, buffer = bufnr })
 
 -- ruataceanvim mapping
+-- change hover actions
 local keys = require("lazyvim.plugins.lsp.keymaps").get() -- Get the default LSP keymaps
 keys[#keys + 1] = { "K", false } -- Set the "K" keymap to false to disable it
-
 map("n", "K", ":RustLsp hover actions<cr>", { silent = true, buffer = bufnr })
+
 map("n", "<leader>rr", ":Cargo run<cr>a", { desc = "Cargo run", silent = true, buffer = bufnr })
 map("n", "<leader>rt", ":Cargo test<cr>a", { desc = "Cargo test", silent = true, buffer = bufnr })
 map("n", "<A-K>", ":RustLsp moveItem up<cr>", { silent = true, buffer = bufnr })
 map("n", "<A-J>", ":RustLsp moveItem down<cr>", { silent = true, buffer = bufnr })
-
-vim.o.winborder = "rounded"
